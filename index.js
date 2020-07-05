1d5cc4b6f7b1b36a86204ad068cc74080cbab933
Olav Fosse <fosseolav@gmail.com>
2020-07-05 23:14:38 +0200

    Finish exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan((tokens, request, response) => {
+    return [
+        tokens.method(request, response),
+        tokens.url(request, response),
+        tokens.status(request, response),
+        tokens.res(request, response, 'content-length'), '-',
+        tokens['response-time'](request, response), 'ms',
+        JSON.stringify(request.body)
+    ].join(' ')
+}))
 
 let persons = [
     {
