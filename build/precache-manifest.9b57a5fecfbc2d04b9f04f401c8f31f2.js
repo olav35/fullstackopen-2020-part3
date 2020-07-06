33dab82499d06c401731eb65d9473a4feb4e659c
Olav Fosse <fosseolav@gmail.com>
2020-07-06 22:33:20 +0200

    Finish exercise 3.11

---
+++ build/precache-manifest.9b57a5fecfbc2d04b9f04f401c8f31f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf40af82403671b096333d4c6c40847d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "428dc1479639092d5efe",
+    "url": "/static/css/main.f3cf7656.chunk.css"
+  },
+  {
+    "revision": "c2d80855719ef2abc918",
+    "url": "/static/js/2.c0506fe1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c0506fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428dc1479639092d5efe",
+    "url": "/static/js/main.5f1f41bd.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan((tokens, request, response) => {return [tokens.method(request, response),
