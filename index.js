5707624eecad8157e960a4574a1dddd8cc9de012
Olav Fosse <fosseolav@gmail.com>
2020-07-06 21:28:46 +0200

    Finish backend of exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan((tokens, request, response) => {
     return [
         tokens.method(request, response),
