0dd0dd0b953345c3593ba466fd9c21c03bd932b8
Olav Fosse <fosseolav@gmail.com>
2020-07-08 18:32:25 +0200

    Convert info route to use MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const persons = require('./models/persons')
 
 const app = express()
 
@@ -16,31 +17,14 @@ app.use(morgan((tokens, request, response) => {return [tokens.method(request, re
     ].join(' ')
 }))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/info', (_, response) => {
-    response.send(`Phonebook has info for 4 people<br><br>${new Date}`)
+    persons.countDocuments({}, (error, count) => {
+        if(error){
+            console.log(error)
+        } else {
+            response.send(`Phonebook has info for ${count} persons<br><br>${new Date}`)
+        }
+    })
 })
 
 app.get('/api/persons', (_, response) => {
