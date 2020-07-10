699b7f4ca2d79909d3f76ff4e23ea308ba988015
Olav Fosse <fosseolav@gmail.com>
2020-07-10 18:31:59 +0200

    Prevent deprecation warnings

--- models/person.js
+++ models/person.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
+
+// Prevent deprecation warnings
+// See https://mongoosejs.com/docs/deprecations.html
 mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true);
 
 const uniqueValidator = require('mongoose-unique-validator')
 
