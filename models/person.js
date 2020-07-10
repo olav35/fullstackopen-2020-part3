c15498ef62830d563dfae2f53b0830b792a57237
Olav Fosse <fosseolav@gmail.com>
2020-07-10 18:11:04 +0200

    Add schema-level validations to person model

--- models/person.js
+++ models/person.js
@@ -3,15 +3,29 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
 
+const uniqueValidator = require('mongoose-unique-validator')
+
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch((error) => console.log(`error connecting to MongoDB: ${error.message}`))
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minlength: 1,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String,
+        minlength: 1,
+        required: true,
+        unique: true
+    }
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
